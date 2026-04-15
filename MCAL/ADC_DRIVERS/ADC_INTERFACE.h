6ce232149c0657d7951f8fe13733bf7955b90fef
Amr-Shinety <156698219+Amr-Shinety@users.noreply.github.com>
2026-04-15 02:30:21 +0200

    MCAL layer
    GPIO and ADC

---
+++ MCAL/ADC_DRIVERS/ADC_INTERFACE.h
@@ -0,0 +1,44 @@
+#ifndef ADC_INTERFACE_H
+#define ADC_INTERFACE_H
+
+
+#include "ADC_PRIVATE.h"
+
+typedef enum
+{
+    AREF,
+    AVCC,
+    INTERNAL
+} adc_vref_t;
+
+typedef enum
+{
+    DIV_2,
+    DIV_4,
+    DIV_8,
+    DIV_16,
+    DIV_32,
+    DIV_64,
+    DIV_128
+} adc_prescaler_t;
+
+typedef enum
+{
+    ADC0,
+    ADC1,
+    ADC2,
+    ADC3,
+    ADC4,
+    ADC5,
+    ADC6,
+    ADC7
+} adc_channel_t;
+
+
+void ADC_init(adc_vref_t vref, adc_prescaler_t prescaler);
+u16 ADC_read(adc_channel_t channel);
+void ADC_freeRunning(adc_channel_t channel);
+f32 ADC_convert_to_v(u16 adc_value, f32 vref);
+f32 ADC_read_v(adc_channel_t channel, f32 vref);
+
+#endif // ADC_INTERFACE_H
---
+++ MCAL/ADC_DRIVERS/ADC_PRIVATE.h
@@ -0,0 +1,13 @@
+#ifndef ADC_PRIVATE_H
+#define ADC_PRIVATE_H
+
+#include "TYPES.h"
+
+#define ADMUX *((volatile u8*)0x27)
+#define ADCSRA  *((volatile u8*)0x26)
+#define ADCH *((volatile u8*)0x25)
+#define ADCL *((volatile u8*)0x24)
+#define SFIOR *((volatile u8*)0x50)
+
+
+#endif /* ADC_PRIVATE_H */
